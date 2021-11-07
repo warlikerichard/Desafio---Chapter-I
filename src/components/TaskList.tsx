787fec3af03a6cbe13e70b2b3f1f6e182b5a5264
warlikerichard <71296712+warlikerichard@users.noreply.github.com>
2021-11-07 19:38:08 -0300

    Add files via upload

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -16,14 +16,28 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask={
+      id : Math.random()*100,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    const taskVar = tasks;
+    if(newTask.title !== ""){
+      taskVar.push(newTask);
+      setTasks(taskVar);
+      setNewTaskTitle("");
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? { ...task, isComplete : !task.isComplete } : task));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
